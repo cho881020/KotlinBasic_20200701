13ced2f3861e423fd343c795682e00725d4121d9
조경진 <kj_cho@idealidea.co.kr>
2020-07-01 20:00:16 +0900

    터치 버튼 클릭 이벤트 부여  - 클릭되면 토스트 띄우기

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
@@ -2,10 +2,22 @@ package kr.co.tjoeun.kotlinbasic_20200701
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        touchBtn.setOnClickListener {
+
+//            touchBtn이 눌리면 실행될 코드를 적는 {  }
+            Toast.makeText(this, "터치 버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
+
 }
