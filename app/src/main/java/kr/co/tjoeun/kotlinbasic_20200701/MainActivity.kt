37a8daf10ec0d955bc56d119c09ce5b916135e3b
조경진 <kj_cho@idealidea.co.kr>
2020-07-01 20:15:31 +0900

    주석 추가

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.kotlinbasic_20200701
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         touchBtn.setOnClickListener {
 
 //            touchBtn이 눌리면 실행될 코드를 적는 {  }
+
+//            Toast를 이용해 사용자에게 안내 메세지.
             Toast.makeText(this, "터치 버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
 
         }
