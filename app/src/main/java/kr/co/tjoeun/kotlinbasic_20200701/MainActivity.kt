4ab47e86857037e35608e039c6c2ae2d9fb7efb8
조경진 <kj_cho@idealidea.co.kr>
2020-07-01 20:58:03 +0900

    코틀린 변수 관련 설명 코드

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
@@ -31,6 +31,26 @@ class MainActivity : AppCompatActivity() {
             Log.d("메인화면", "둘째 버튼 눌림")
         }
 
+//        토스트띄우기 버튼이 눌리면
+        showToastBtn.setOnClickListener {
+
+//            EditText에 입력된 내용을 받자. (String으로 저장해두자)
+//            대입값이 String이면 => 변수 타입도 자동으로 String으로 결정됨.
+            val inputContent = inputContentEdt.text.toString()
+
+//            상수 (val) 와 변수(var) 의 차이
+//            val input : String
+//            input = "테스트"
+//            input = "테스트2"
+//            var input2 : String
+//            input2 = "입력테스트"
+//            input2 = "입력테스트2"
+
+//            10은 정수. 정수를 num1에 대입. => num1은 Int로 결정.
+//            val num1 = 10
+
+        }
+
     }
 
 }
